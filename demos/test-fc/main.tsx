67aaf96fa4fbfcada5911a910f6895426af37bd9
88Codecat <mn3377218@163.com>
2025-05-13 15:46:49 +0800

    feat: synchronous scheduling

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,18 +1,14 @@
-import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const a: React.ReactElement = React.createElement('h1', { children: '123' });
 const jsx = (
 	<div>
-		<p>egeaeagag</p>
-		<span>hello my-react</span>
+		<span>hello</span>
+		<span>my-react</span>
 	</div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(jsx);
-// root.render(a);
-console.log('打印的react元素结果', a);
+root.render(<App />);
 console.log('打印的<App />结果：', <App />);
 console.log('打印的jsx结果:', jsx);
